d0d87e3ef4419fe77a9023e59a7f0af8bf92d600
NoWind104 <1605128429@qq.com>
2023-09-08 10:27:11 +0800

    'v1.0'

---
+++ 双击登录校园网.py
@@ -0,0 +1,19 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/9/7 11：33
+# @Author : 枫 QQ1605128429 group794409253
+# @File : AutoLogin
+# @Project : SchoolWebAutoLogin
+#%40telecom,%40cmcc,%40unicom,
+
+import requests    # 用于向目标网站发送请求
+import os
+
+f = open("D:/登录配置.txt", "r")
+user=f.readline()
+password=f.readline()
+op=f.readline()
+f.close()
+url = 'http://172.16.1.11/drcom/login?callback=dr1003&DDDDD='+user[:-1]+op[:-1]+'&upass='+password[:-1]+'&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1.3&v=10163&lang=zh'
+response = requests.get(url).status_code  # 直接利用 GET 方式请求这个 URL 同时获取状态码
+print("状态码{}".format(response))  # 打印状态码
+os.system("pause")
