05246f1424d895fd1dcb0079bc2eaeb0fb48088a
NoWind104 <1605128429@qq.com>
2023-09-09 19:20:24 +0800

    修改内容

--- gxust_login.py
+++ gxust_login.py
@@ -7,7 +7,7 @@ user=f.readline()
 password=f.readline()
 op=f.readline()
 f.close()
-url = 'http://172.16.1.11/drcom/login?callback=dr1003&DDDDD='+user[:-1]+op[:-1]+'&upass='+password[:-1]+'&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1.3&v=10163&lang=zh'
+url = 'http://172.16.1.11/drcom/login?callback=dr1003&DDDDD='+user[9:-1]+op[9:-1]+'&upass='+password[9:-1]+'&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1.3&v=10163&lang=zh'
 response = requests.get(url).status_code  # 直接利用 GET 方式请求这个 URL 同时获取状态码
 print("状态码{}".format(response))  # 打印状态码
 os.system("pause")
